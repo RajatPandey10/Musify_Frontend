f043ffbb409ad9ecc9406a6e63d55c183ffe9933
RajatPandey10 <pandeyrajat9971@gmail.com>
2026-02-25 23:56:05 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Login from './components/Login.jsx'
+import Register from './components/Register.jsx'
+import './App.css'
+function App() {
+  
+
+  return (
+    <div>
+      <Login/>
+      <Register />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Register.jsx
@@ -0,0 +1,92 @@
+import { assets } from "../assets/assets";
+const Register = ()=>{
+    return (
+        <div className='min-h-screen bg-gradient-to-r from-green-900 via-black to-green-900 flex items-center justify-center p-4'>
+            <div className='max-w-md w-full space-y-8'>
+                {/* Header */}
+                <div className="text-center">
+                     <div className='flex items-center justify-center mb-6'>
+                        <div className='flex items-center justify-center mb-6'>
+                            <img src={assets.logo} alt='logo' className='w-20 h-20'></img>
+                            <h1 className='ml-3 text-3xl font-bold text-white'>Musify</h1>
+                        </div>
+                     </div>
+
+                     <h2 className='text-2xl font-bold text-white mb-2 ml-8'>Join Musify</h2>
+                     <p className='text-gray-300 ml-8'>
+                        Create your account for start listening
+                     </p>
+
+                </div>
+
+                {/* Register from */}
+                <div className='bg-gray-900/80 backdrop-blue-lg rounded-2xl p-8 shadow-2xl border-gray-700 '>
+                    <form className='space-y-6'>
+                        {/* email field */}
+                        <div>
+                            <label htmlFor='email' className='block text-sm font-medium text-gray-200 mb-2'>
+                                Email Address
+                            </label>
+                            <input type='text' name='email' autoComplete='email' required 
+                                className='block w-full px-4 py-3 border-gray-600 rounded-lg bg-gray-800/50 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent transition-all duration-200'
+                                placeholder='Enter your email'
+                                ></input>
+                        </div>
+
+                        {/* password field */}
+                        <div>
+                            <label htmlFor='password' className='block text-sm font-medium text-gray-200 mb-2'>
+                                Password
+                            </label>
+                            <input type='password' name='password' id='password' autoComplete='new-password' required 
+                                className='block w-full px-4 py-3 border-gray-600 rounded-lg bg-gray-800/50 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent transition-all duration-200'
+                                placeholder='Create a password'
+                                ></input>
+
+                        </div>
+
+                         {/* confirm password field */}
+                        <div>
+                            <label htmlFor='confirmPassword' className='block text-sm font-medium text-gray-200 mb-2'>
+                                Confirm Password
+                            </label>
+                            <input type='password' name='confirmPassword' id='password' autoComplete='new-password' required 
+                                className='block w-full px-4 py-3 border-gray-600 rounded-lg bg-gray-800/50 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent transition-all duration-200'
+                                placeholder='Confirm your  password'
+                                ></input>
+
+                        </div>
+
+                        {/* Submit button */}
+
+                        <button className='w-full flex justify-center py-3 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 disabled:opacity-50 disabled-cursor-not-allowed transition-all duration-200 transform hover:scale-105'>
+                            Register
+                        </button>
+
+                    </form>
+
+                    {/* Switch to login */}
+
+                    <div className='mt-6 text-center'>
+                        <p className='text-sm text-gray-400'>
+                            Already have an account?
+                            <buttom className=' text-green-400 hover:text-green-300 font-medium transition-colors cursor-pointer'>
+                                Sign in here
+                            </buttom>
+                        </p>
+                    </div>
+                    {/* terms and condition */}
+                    <div className='mt-4 text-center'>
+                        <p className='text-xs text-gray-400'>
+                            By creating an account, you agree to our Terms of Service and Privacy Policy.
+                        </p>
+
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Register;
