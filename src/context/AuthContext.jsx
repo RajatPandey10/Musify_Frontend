79ad7bc0d84897b98c66c7b2b196b85bd41c8b6b
RajatPandey10 <pandeyrajat9971@gmail.com>
2026-03-23 22:44:52 +0530

    changed  url

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import {createContext,useContext,useState,useEffect} from 'react';
 
 
 import axios from "axios";
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://musify-backend-2ofg.onrender.com";
 
 export const AuthContext = createContext();
 
